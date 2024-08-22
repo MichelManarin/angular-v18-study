3440e445352a433847b20b10ca94b8b92d17dc53
michel.manarin <michel.manarin@nvsecurity.com.br>
2024-08-22 15:48:03 -0300

    feat: parte 2 estudo

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../_models/user';
 
 @Component({
   selector: 'app-users',
@@ -6,9 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-  users: String[] = [
-    "Vinicius",
-    "Julio",
-    "Larissa"
+  // users: String[] = [
+  //   "Vinicius",
+  //   "Julio",
+  //   "Larissa"
+  // ]
+  userSelecionado: User | undefined;
+  users: User[] = [
+    { nome: "Vinicius", idade: 25 },
+    { nome: "Julio", idade: 19},
+    { nome: "Larissa", idade: 22 },
+    { nome: "Maria", idade: 45 },
   ]
+
+  infoUserSelecionado(user: User){
+    this.userSelecionado = user;
+  }
 }
